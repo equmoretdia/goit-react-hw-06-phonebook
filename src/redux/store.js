9fa37a399ff66b743c7fc37ee1db5fe82309966e
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-05 16:20:38 +0100

    filterSlice is added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
-import { filterSlice } from './filterSlice';
+import { filterReducer } from './filterSlice';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
-    filter: filterSlice.reducer,
+    filter: filterReducer,
   },
 });
